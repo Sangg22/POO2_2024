ccb0cade5654c58e6cfd5091c4036b015deadf90
Sangg22 <142357065+Sangg22@users.noreply.github.com>
2024-05-31 05:52:08 -0600

    Update main.cpp
    Incluir las nuevas clases en el main y aparte ejemplos de como funciona el polimorfismo y la agregación al cliente.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
+#include "Suite.h"
 #include "Habitacion.h"
-int main() {
-    // Ejemplo de uso de la clase Habitacion
-    Habitacion hab1(101, "Suite", 4, 150.0, true);
-    hab1.imprimeDatos();
-    cout << endl;
+#include "Reserva.h"
+#include "Cliente.h"
+#include "Estandar.h"
 
-    // Ejemplo de uso de la clase Suite
-    Suite suite1(101, "Suite", 4, 150.0, true, 100.0);
-    suite1.imprimeDatos();
+int main() {
+    //Establecer valores a la habitación estandar y la suite
+    Estandar habitacion1(101, "Estandar", 4, 150.0, true);
+    Suite habitacion2(102, "Suite", 4, 250.0, true, 100.0);
+
+    //Establecer valores al cliente y crear dos reservas para agregar
+    Cliente cliente1("Santiago Gonzalez", "santiglz79@gmail.com");
+    Reserva reserva1("2024-06-01", "2024-06-10");
+    Reserva reserva2("2024-07-10", "2024-07-19");
+
+    //Agregar reservas
+    cliente1.agregarReserva(reserva1);
+    cliente1.agregarReserva(reserva2);
+
+    //Imprimir información del cliente
+    cliente1.imprimirDatos();
+    //Imprimir habitaciones
+    habitacion1.imprimeDatos();
+    habitacion2.imprimeDatos();
 
     return 0;
 }
