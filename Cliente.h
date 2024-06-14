88b80203c4996046b62a4266266c43e27c849ba3
Sangg22 <142357065+Sangg22@users.noreply.github.com>
2024-06-14 08:54:02 -0600

    Update Cliente.h

--- Cliente.h
+++ Cliente.h
@@ -1,3 +1,17 @@
+/*
+ *
+ * Proyecto Gestor de un hotel clase Cliente
+ * Santiago Isai González Arista
+ * A01712184
+ * 13/06/2024
+ *
+ * Esta clase define el cliente al cual estaríámos atendiendo, aqui el cliente
+ * tiene 3 atributos siendo su nombre y su correo, al igual se crea un vector
+ * de reservaciones para que el cliente puede tener más de una reservación,
+ * por lo que tiene un metodo para agregar reservas y uno para mostrar su
+ * información.
+ */
+
 #ifndef CLIENTE_H_
 #define CLIENTE_H_
 
@@ -15,7 +29,7 @@ class Cliente{
     private:
         string nombre;
         string correo;
-        vector<Reserva> reservas;
+        vector<Reserva*> reservas;
     //Metodos
     public:
         //Constructores
@@ -28,17 +42,27 @@ class Cliente{
         string getNombre();
         string getCorreo();
         //Función para agregar reservas e imprimir los datos
-        void agregarReserva(Reserva);
+        void agregarReserva(Reserva* reservas);
         void imprimirDatos();
 };
 
-//Inicializar valores
+/**
+ * Cliente constructor vacío
+ * Inicializa el objeto con los atributos default
+ * @param 
+ * @return
+ */
 Cliente::Cliente(){
     nombre = "";
     correo = "";
 }
 
-//Definir
+/**
+ * Cliente constructor con parámetros 
+ * Define parametros de del cliente
+ * @param string(nombre, correo)
+ * @return
+ */
 Cliente::Cliente(string nom, string cor){
     nombre = nom;
     correo = cor;
@@ -62,22 +86,32 @@ string Cliente::getCorreo(){
     return correo;
 }
 
-//Usar push_back para agregar reservas
-void Cliente::agregarReserva(Reserva reserva){
+/**
+ * Funcion agregarReserva(Reserva* reserva)
+ * Función que sirve para agregar reservas en el vector usando push_back
+ * para ir recorriendo.
+ * @param  Reserva* reserva
+ * @return agregar reservas al vector
+ */
+void Cliente::agregarReserva(Reserva* reserva){
     reservas.push_back(reserva);
 }
 
-//Imprimir datos
+/**
+ * Funcion imprimirDatos() que devuelve los datos del cliente
+ * Contiene un for para ir imprimiendo todas las reservas que se han agregado.
+ * @param 
+ * @return impresión de los datos dados y las reservaciones con un for
+ */
 void Cliente::imprimirDatos(){
     cout << "CLIENTE:" <<endl;
     cout << "Nombre: " << nombre << endl;
     cout << "Correo: " << correo << endl;
-    int dias_estancia = 0;
-    for (Reserva& reserva : reservas) {
-        dias_estancia += reserva.getDias(); // Asumiendo que Reserva tiene un método getDiasEstancia()
-    }
-    
-    cout << "Dias de estancia: " << dias_estancia << endl;
+    cout << "Reservaciones: " << endl;
+    for (Reserva* reserva : reservas) {
+        reserva->imprimeDatos();
+        cout << "-----------------------------------" << endl;
+}
 
 }
 
