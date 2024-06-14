afa0489f830846a063a1833c99cbc640d9f30c01
Sangg22 <142357065+Sangg22@users.noreply.github.com>
2024-06-14 08:55:58 -0600

    Update Estandar.h

--- Estandar.h
+++ Estandar.h
@@ -1,3 +1,16 @@
+/*
+ *
+ * Proyecto Gestor de un hotel clase Estandar
+ * Santiago Isai González Arista
+ * A01712184
+ * 13/06/2024
+ *
+ * Esta clase define una habitacón del tipo estandar que hereda de Habitación.
+ * Lo que representaría el tipo de habitación más básico siendo que en este
+ * el precio total sería solo el precio base por los dias de estancia.
+ * Por lo tanto esta clase es usada para crear habitaciones estandar.
+ */
+
 #ifndef ESTANDAR_H_
 #define ESTANDAR_H_
 #include "Habitacion.h"
@@ -6,33 +19,56 @@ class Estandar : public Habitacion{
     //Metodos    
     public:
         //Constructores
+        //Constructor vacio
         Estandar();
-        Estandar(int num, string tip, int cap, int prec, bool disp);
+        //Constructor que recibre parametros
+        Estandar(int num, string tip, int cap, int prec, int dias);
 
-        //funcion de calcular precio e imprimir datos y polimorfismo
-        int calculaPrecioTotal(int dias) override;
+        //funcion de calcular precio e imprimir datos usando override para
+        //sobreescribir la función
+        int calculaPrecioTotal() override;
         void imprimeDatos() override;
 };
 
-//Inicializar valores en constructor vacio
+/**
+ * Estandar constructor vacío junto constructor vacio de la clase Padre
+ * Inicializa el objeto con los atributos default
+ * @param 
+ * @return
+ */
+
 Estandar::Estandar() : Habitacion(){
 }
 
-//Establecer parametros de la clase hija al igual que los de la padre
-Estandar::Estandar(int num, string tip, int cap, int prec, bool disp) : Habitacion(num, tip, cap, prec, disp){
+/**
+ * Estandar constructor con parámetros y constructor de clase padre
+ * parametros.
+ * @param Solo usa los parametros de la clase madre
+ * @return
+ */
+Estandar::Estandar(int num, string tip, int cap, int prec, int dias) : 
+Habitacion(num, tip, cap, prec, dias){
 }
 
-//Modificar función en la que ahora se hará una suma para saber el precio total
-int Estandar::calculaPrecioTotal(int dias){
-    return dias * getPrecio_base();
+/**
+ * Funcion calcularPrecioTotal() que devuelve el precio total
+ * parametros.
+ * @param 
+ * @return int(PrecioTotal )
+ */
+int Estandar::calculaPrecioTotal(){
+    return  (precio_base * dias_estancia);
 }
 
-//Imprimir Datos
+/**
+ * Funcion imprimeDatos() que devuelve el precio total
+ * parametros.
+ * @param 
+ * @return Impresión de datos que se le dieron
+ */
 void Estandar::imprimeDatos(){
-    Habitacion::imprimeDatos();
     cout << "Tipo de habitacion: " << getTipo() << endl;
     cout << "Numero de habitacion: " << getNumero() << endl;
-    cout << "Precio total de la habitacion estandar: " << calculaPrecioTotal(1) << endl;
-    cout << "Disponibilidad: " << (getDisponibilidad() ? "Disponible" : "No Disponible") << endl;
+    cout << "Precio total de la habitacion : " << calculaPrecioTotal() << endl;
 }
 #endif
